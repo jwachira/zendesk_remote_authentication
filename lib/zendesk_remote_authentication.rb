ded02c04491fd0ae127cc1d15fc7f2b6f7f8a54c
James Wachira <jamomathenge@gmail.com>
2010-11-04 22:39:30 -0700

    -adding zendesk token

--- lib/zendesk_remote_authentication.rb
+++ lib/zendesk_remote_authentication.rb
@@ -26,11 +26,12 @@ module RemoteAuthenticationHelper
    def zendesk_remote_authentication_url(params)
       raise ArgumentError.new("You must provide name and email address") unless (params[:name] && params[:email])
       
+      token       = Zendesk::RemoteAuthentication.token
       timestamp   = params[:timestamp] || Time.now.to_i.to_s
       name        = params[:name]
       email       = params[:email].force_utf8
       external_id = params[:external_id]
-      hash        = Digest::MD5.hexdigest(name + email + external_id + Token + now)
+      hash        = Digest::MD5.hexdigest(name + email + external_id + token + now)
       back        = params[:return_to]
 
       auth_params = [
